03080f918829bb5d93e7c4f981136aaf63df0da4
aaronwolen <aaron@wolen.com>
2013-06-30 17:53:53 -0400

    Don't print abstract

--- bibjekyll.rb
+++ bibjekyll.rb
@@ -13,7 +13,7 @@ module Jekyll
 
   class BibtexTag < Liquid::Tag
     # The options that are passed to bibtex2html
-    Options = "-nofooter -noheader -use-table -nokeywords -nokeys -nodoc -dl"
+    Options = "-nofooter -noheader -use-table -nokeywords -nokeys -nodoc -no-abstract"
 
     def split_params(params)
       params.split(" ").map(&:strip)
